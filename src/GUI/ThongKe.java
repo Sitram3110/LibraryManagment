39277222eab817d5e98d7cb2ec9dfc41630484da
Sitram3110 <basitram1234@gmail.com>
2023-10-19 16:57:31 +0700

    update

--- src/GUI/ThongKe.java
+++ src/GUI/ThongKe.java
@@ -6,7 +6,7 @@ import java.awt.*;
 
 public class ThongKe extends JFrame {
     private JPanel readerStatic, bookStatic, fineStatic;
-    private JPanel readerNorth, readerSouth, ReaderWest, ReaderCenter, ReaderEast, bookNorth, bookSouth, bookEast,
+    private JPanel readerNorth, readerSouth, readerWest, readerCenter, readerEast, bookNorth, bookSouth, bookEast,
             bookCenter, bookWest, fineSouth, fineEast, fineNorth, fineCenter, fineWest;
     private JTable readerTable, bookTable, fineTable;
 
@@ -21,30 +21,37 @@ public class ThongKe extends JFrame {
         setSize(900, 500);
         setVisible(true);
         setLayout(new BorderLayout());
-        readerStatic.setLayout(new BorderLayout());
-        bookStatic.setLayout(new BorderLayout());
-        fineStatic.setLayout(new BorderLayout());
+
+        readerStatic = new JPanel(); // Initialize the panels
+        bookStatic = new JPanel();
+        fineStatic = new JPanel();
 
         // set Panel into BorderLayout
         // readerStatic
+        readerStatic.setLayout(new BorderLayout());
         readerStatic.add(readerNorth, BorderLayout.NORTH);
         readerStatic.add(readerSouth, BorderLayout.SOUTH);
-        readerStatic.add(ReaderEast, BorderLayout.EAST);
-        readerStatic.add(ReaderWest, BorderLayout.WEST);
-        readerStatic.add(ReaderCenter, BorderLayout.CENTER);
-        ReaderCenter.setPreferredSize(new Dimension(700, 300));
+        readerStatic.add(readerEast, BorderLayout.EAST);
+        readerStatic.add(readerWest, BorderLayout.WEST);
+        readerStatic.add(readerCenter, BorderLayout.CENTER);
+        readerCenter.setPreferredSize(new Dimension(700, 300));
+
         // bookStatic
+        bookStatic.setLayout(new BorderLayout());
         bookStatic.add(bookNorth, BorderLayout.NORTH);
         bookStatic.add(bookSouth, BorderLayout.SOUTH);
         bookStatic.add(bookEast, BorderLayout.EAST);
         bookStatic.add(bookWest, BorderLayout.WEST);
         bookStatic.add(bookCenter, BorderLayout.CENTER);
+
         // fineStatic
+        fineStatic.setLayout(new BorderLayout());
         fineStatic.add(fineNorth, BorderLayout.NORTH);
         fineStatic.add(fineSouth, BorderLayout.SOUTH);
         fineStatic.add(fineEast, BorderLayout.EAST);
         fineStatic.add(fineWest, BorderLayout.WEST);
         fineStatic.add(fineCenter, BorderLayout.CENTER);
+
         // create table
         // create contain panel
         JPanel panelReader = new JPanel();
@@ -53,9 +60,8 @@ public class ThongKe extends JFrame {
         // reader
         String[] readerCol = { "employee_id", "user_name", "pass", "position", "surname", "name", "gender", "birth",
                 "email", "phone", "address" };
-        // JTable readerTable = new JTable(tableModel);
-        DefaultTableModel tableModel = new DefaultTableModel(readerCol, 0);
-        readerTable = new JTable(tableModel);
+        // DefaultTableModel tableModel = new DefaultTableModel(readerCol, 0);
+        // readerTable = new JTable(tableModel);
         readerTable.getTableHeader().setResizingAllowed(false);
         panelReader.add(readerTable);
         readerSouth.setLayout(new BorderLayout());
@@ -67,10 +73,9 @@ public class ThongKe extends JFrame {
         tabbedPane.addTab("Thong Ke Sach", bookStatic);
         tabbedPane.addTab("Thong Ke Tine Phat", fineStatic);
         add(tabbedPane);
-
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new QuanLyDocGia());
+        SwingUtilities.invokeLater(() -> new ThongKe());
     }
 }
